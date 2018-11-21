bc9e17ecdf551bb6c0e9e8dce12320c20f71fe5b
Mcfe <luzac@163.com>
2018-11-21 12:34:39 +0800

    FirstAdd

---
+++ EACoverter/Program.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using LitJson;
+
+namespace EACoverter {
+    public class FilterConfig {
+        public string SaveFileName;
+        public List<string> FilterList;
+        public bool NeedToRemove = false;
+        public PredictionType PType = PredictionType.ENROUT;
+        public bool IsPDF = true;
+    }
+
+    public class EAConfig {
+        public List<string> DeleteList;
+        public List<FilterConfig> FilterList;
+    }
+
+    public enum PredictionType {
+        ENROUT,
+        AIRPORT
+    }
+
+    class Prediction {
+        public PredictionType PType = PredictionType.ENROUT;
+        public string KeyVal = string.Empty;
+        private List<string> content = null;
+        public List<string> Content {
+            get { return content; }
+            set {
+                content = value;
+            }
+        }
+
+        public List<int> RNPList = new List<int>();
+    }
+
+    class Program {
+
+        public static List<Prediction> Predictions = new List<Prediction>();
+        public static DirectoryInfo ResultDirectoryInfo = null;
+        private static EAConfig MyConfig = null;
+
+        static void Main(string[] args) {
+            LoadData();
+            Console.ReadLine();
+        }
+
+        static void LoadData() {
+            Predictions.Clear();
+
+            string resultFolder = Directory.GetCurrentDirectory() + "\\Result";
+            if (!Directory.Exists(resultFolder))
+                Directory.CreateDirectory(resultFolder);
+            ResultDirectoryInfo = new DirectoryInfo(resultFolder);
+            ResultDirectoryInfo.GetFiles().ToList().ForEach(file => file.Delete());
+
+            string tmpFolder = Directory.GetCurrentDirectory() + "\\Tmp";
+            if (Directory.Exists(tmpFolder))
+                Directory.Delete(tmpFolder, true);
+
+            DirectoryInfo currentDirInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+            List<FileInfo> files = currentDirInfo.GetFiles().ToList();
+            FileInfo firstValidZipFile = files.FirstOrDefault(fileInfo => fileInfo.Name.EndsWith(".zip"));
+            if (firstValidZipFile == null) {
+                Console.WriteLine("No valid zip is found!");
+                return;
+            }
+
+            ZipFile.ExtractToDirectory(firstValidZipFile.Name, tmpFolder);
+            DirectoryInfo tmpDirectory = new DirectoryInfo(tmpFolder);
+            files = tmpDirectory.GetFiles().ToList();
+            files.ForEach(fileInfo => {
+                if (fileInfo.Name.EndsWith(".txt")) {
+                    Console.WriteLine(fileInfo.Name);
+                    ProcessFile(fileInfo);
+                }
+            });
+            Console.WriteLine("TotalFileCount:" + files.Count);
+            Console.WriteLine("ValidFileCount:" + Predictions.Count);
+
+            string cfgFilePath = Directory.GetCurrentDirectory() + "\\EAConfig.cfg";
+            if (!File.Exists(cfgFilePath)) {
+                Console.WriteLine("EAConfig.cfg not found!");
+                return;
+            }
+            else {
+                DirectoryInfo cfgFileInfo = new DirectoryInfo(cfgFilePath);
+                StreamReader sR = new StreamReader(cfgFileInfo.FullName);
+                string content = string.Empty;
+                string line = string.Empty;
+                while ((line = sR.ReadLine()) != null) {
+                    Console.WriteLine(line);
+                    content += line;
+                }
+                sR.Close();
+                MyConfig = JsonMapper.ToObject<EAConfig>(content);
+            }
+            DoFilter();
+        }
+
+        static void ProcessFile(FileInfo validFile) {
+            StreamReader sR = new StreamReader(validFile.FullName);
+            List<string> content = new List<string>();
+            List<int> RNPList = new List<int>();
+            string line = string.Empty;
+            while ((line = sR.ReadLine()) != null) {
+                Console.WriteLine(line);
+                content.Add(line);
+                if (line.StartsWith("RNP")) {
+                    int colonIndex = line.IndexOf(":");
+                    string tmpString = line.Substring(3, colonIndex - 3);
+                    float rnpStandardVal = float.MinValue;
+                    if (float.TryParse(tmpString, out rnpStandardVal))
+                        RNPList.Add((int)rnpStandardVal);
+                }
+            }
+            sR.Close();
+
+            if (content.Count == 0) {
+                Console.WriteLine("Prediction file is empty:{0}", validFile.Name);
+                return;
+            }
+
+            string keyString = content.FirstOrDefault(str => {
+                return str.Contains("ENROUT") || str.Contains("AIRPORT");
+            });
+
+            if (string.IsNullOrEmpty(keyString)) {
+                Console.WriteLine("Prediction file has no key(ENROUT | AIRPORT):{0}", validFile.Name);
+                return;
+            }
+
+            string[] result = keyString.Split(':');
+            if (result == null || result.Length != 2) {
+                Console.WriteLine("Prediction file's key has format error:{0}", validFile.Name);
+                return;
+            }
+
+            Prediction newPrediction = new Prediction();
+            newPrediction.PType = result[0].Trim() == "ENROUT" ? PredictionType.ENROUT : PredictionType.AIRPORT;
+            newPrediction.KeyVal = result[1].Trim();
+            newPrediction.Content = content;
+            newPrediction.RNPList = RNPList;
+            Predictions.Add(newPrediction);
+        }
+
+        static void DoFilter() {
+            Console.WriteLine(JsonMapper.ToJson(MyConfig));
+            DoRemove(MyConfig.DeleteList);
+            //GentBriefView(true);
+            //GentBriefView(false);
+            MyConfig.FilterList.ForEach(filter => {
+                DoFilter(filter.FilterList, filter.SaveFileName, filter.PType, filter.NeedToRemove, filter.IsPDF);
+            });
+            Console.WriteLine("Convert Finished!");
+            //DoRemove(new List<string> { "L642", "M771", "Q1", "Q15" });
+            //DoFilter(new List<string> { "A345", "B451" }, "ZBLA", PredictionType.ENROUT, true);
+            //DoFilter(new List<string> { "H89", "H90", "H117", "W146", "W162" }, "ZPLJ", PredictionType.ENROUT, true);
+            //DoFilter(new List<string> { "M503" }, "M503", PredictionType.ENROUT, true);
+            //DoFilter(new List<string> { "B215" }, "B215", PredictionType.ENROUT, false);
+            //DoFilter(new List<string>(), "ZWWW", PredictionType.ENROUT, true);
+            //DoFilter(new List<string>(), "RAIM", PredictionType.AIRPORT, true);
+        }
+
+        private static void GentBriefView(bool isAirport) {
+            //Present all airport prediction
+            PredictionType targetType = isAirport ? PredictionType.AIRPORT : PredictionType.ENROUT;
+            string targetFileName = isAirport ? "AirportBriefView.pdf" : "EnroutBriefView.pdf";
+            var allValidPredicts = Predictions.Where(pred => pred.PType == targetType);
+            List<Prediction> predicts_EX = new List<Prediction>();
+            List<Prediction> predicts_Normal = new List<Prediction>();
+            allValidPredicts.ToList().ForEach(pred => {
+                if (pred.Content.Any(line => line.Contains("except for")))
+                    predicts_EX.Add(pred);
+                else
+                    predicts_Normal.Add(pred);
+            });
+
+            TxtToPDFConverter.GenerateBriefView(isAirport, predicts_EX, predicts_Normal,
+                string.Format("{0}\\{1}", ResultDirectoryInfo.FullName, targetFileName));
+        }
+
+        static void DoFilter(List<string> keys, string targetFileName, PredictionType targetType, bool needToRemove, bool isPDF) {
+            Debug.Assert(keys != null);
+            List<Prediction> validPreds = new List<Prediction>();
+
+            if (keys.Count == 0)
+                validPreds.AddRange(Predictions.Where(pred => pred.PType == targetType));
+            else
+                validPreds = Predictions.Where(pred => keys.Contains(pred.KeyVal)).ToList();
+
+            if (validPreds.Count == 0)
+                return;
+
+            //TwoImplements
+            if (ResultDirectoryInfo != null && !string.IsNullOrEmpty(targetFileName)) {
+                if (targetType == PredictionType.AIRPORT) {
+                    if (!isPDF) {
+                        string targetName = string.Format("{0}\\{1}.txt", ResultDirectoryInfo.FullName, targetFileName);
+                        StreamWriter sw = new StreamWriter(targetName);
+                        validPreds.ForEach(pred => {
+                            pred.Content.ForEach(singleContent => sw.WriteLine(singleContent));
+                            sw.WriteLine("");
+                        });
+                        sw.Close();
+                        Console.WriteLine(string.Format("{0}.txt is generated!", targetName));
+                    }
+                    else {
+                        string targetName = string.Format("{0}\\{1}.pdf", ResultDirectoryInfo.FullName, targetFileName);
+                        TxtToPDFConverter.Convert(validPreds, targetName);
+                    }
+                }
+                else {
+                    var allValidPredicts = validPreds.Where(pred => pred.PType == targetType);
+                    List<Prediction> predicts_EX = new List<Prediction>();
+                    List<Prediction> predicts_Normal = new List<Prediction>();
+                    allValidPredicts.ToList().ForEach(pred => {
+                        if (pred.Content.Any(line => line.Contains("except for")))
+                            predicts_EX.Add(pred);
+                        else
+                            predicts_Normal.Add(pred);
+                    });
+
+                    if (!isPDF) {
+                        var result = TxtToPDFConverter.GenerateBriefContents(false, predicts_EX, predicts_Normal);
+                        string targetName = string.Format("{0}\\{1}.txt", ResultDirectoryInfo.FullName, targetFileName);
+                        StreamWriter sw = new StreamWriter(targetName);
+                        result.ForEach(line => {
+                            sw.WriteLine(line);
+                        });
+                        sw.Close();
+                        Console.WriteLine(string.Format("{0}.txt is generated!", targetName));
+                    }
+                    else {
+                        string targetName = string.Format("{0}\\{1}.pdf", ResultDirectoryInfo.FullName, targetFileName);
+                        TxtToPDFConverter.GenerateBriefView(false, predicts_EX, predicts_Normal, targetName);
+                    }
+                }
+            }
+            if (needToRemove)
+                validPreds.ForEach(pred => Predictions.Remove(pred));
+            Console.WriteLine("Prediction Count Left:" + Predictions.Count);
+        }
+
+        static void DoRemove(List<string> keys) {
+            if (keys == null)
+                return;
+            Predicate<Prediction> toRemoveCondition = pred => { return keys.Contains(pred.KeyVal); };
+            Predictions.RemoveAll(toRemoveCondition);
+        }
+    }
+}
---
+++ EACoverter/TxtToPDFConverter.cs
@@ -0,0 +1,136 @@
+﻿using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace EACoverter {
+    class TxtToPDFConverter {
+        public static void Convert(List<Prediction> preds, string targetFileName) {
+            //Document document = CreateDocument();
+            Document document = CreateDocument(preds);
+            document.UseCmykColor = true;
+            const bool unicode = false;
+            const PdfFontEmbedding embedding = PdfFontEmbedding.Always;
+            PdfDocumentRenderer pdfRenderer = new PdfDocumentRenderer(unicode, embedding);
+            pdfRenderer.Document = document;
+            pdfRenderer.RenderDocument();
+            pdfRenderer.PdfDocument.Save(targetFileName);
+            Process.Start(targetFileName);
+            Console.WriteLine(string.Format("{0}.pdf is generated!", targetFileName));
+        }
+
+        public static List<string> GenerateBriefContents(bool isAirport, List<Prediction> predsEx, List<Prediction> predsNormal) {
+            List<string> result = new List<string>();
+            if (predsEx.Count == 0 && predsNormal.Count == 0) {
+                Console.WriteLine("No valid airport prediction!");
+                return result;
+            }
+
+            predsEx.ForEach(pred => {
+                result.AddRange(pred.Content);
+                result.Add("");
+            });
+
+            result.Add("------------------------------------------------------------------\r\n");
+
+            //if (predsNormal.Count != 0) {
+            //    int maxContentSize = isAirport ? 5 : 6;
+            //    string prefixName = isAirport ? "AIRPORT" : "ENROUT";
+            //    var firstCompletePrediction = predsNormal.FirstOrDefault(pred => pred.Content.Count >= maxContentSize);
+            //    if (firstCompletePrediction == null)
+            //        firstCompletePrediction = predsNormal[0];
+
+            //    result.Add(firstCompletePrediction.Content[0]);
+            //    result.Add(firstCompletePrediction.Content[maxContentSize - 3]);
+            //    result.Add(firstCompletePrediction.Content[maxContentSize - 2]);
+            //    if (firstCompletePrediction.Content.Count >= maxContentSize)
+            //        result.Add(firstCompletePrediction.Content[maxContentSize - 1]);
+
+            //    predsNormal.ForEach(pred => {
+            //        result.Add(string.Format("{0}:{1}", prefixName, pred.KeyVal));
+            //    });
+            //}
+
+            if (predsNormal.Count != 0) {
+                int maxContentSize = isAirport ? 5 : 6;
+                string prefixName = isAirport ? "AIRPORT" : "ENROUT";
+                var firstCompletePrediction = predsNormal.FirstOrDefault(pred => pred.Content.Count >= maxContentSize);
+                if (firstCompletePrediction == null)
+                    firstCompletePrediction = predsNormal[0];
+
+                result.Add(firstCompletePrediction.Content[0]);
+                result.Add(firstCompletePrediction.Content[maxContentSize - 3]);
+
+                var finalKeys = predsNormal.Select(pred => pred.RNPList).Aggregate((a, b) => {
+                    List<int> tmp = new List<int>();
+                    tmp.AddRange(a);
+                    tmp.AddRange(b);
+                    return tmp.Distinct().ToList();
+                });
+
+                finalKeys.OrderBy(key => key).ToList().ForEach(rnpKey => {
+                    var targetPreds = predsNormal.Where(pred => pred.RNPList.Contains(rnpKey)).ToList();
+
+                    if (targetPreds.Count == 0)
+                        return;
+
+                    string title = targetPreds[0].Content.FirstOrDefault(line => line.StartsWith(string.Format("RNP{0}.00", rnpKey)));
+                    result.Add(title);
+                    targetPreds.ForEach(pred => {
+                        result.Add(string.Format("{0}:{1}", prefixName, pred.KeyVal));
+                    });
+                    result.Add("\r\n");
+                });
+            }
+            return result;
+        }
+
+        public static void GenerateBriefView(bool isAirport, List<Prediction> predsEx, List<Prediction> predsNormal, string targetFileName) {
+            Document document = new Document();
+            Section section = document.AddSection();
+            Paragraph paragraph = section.AddParagraph();
+            paragraph.Format.Font.Color = Color.FromCmyk(100, 30, 20, 50);
+
+            var result = GenerateBriefContents(isAirport, predsEx, predsNormal);
+            result.ForEach(line => {
+                paragraph.AddFormattedText(line + "\r\n", TextFormat.Bold);
+            });
+
+            document.UseCmykColor = true;
+            const bool unicode = false;
+            const PdfFontEmbedding embedding = PdfFontEmbedding.Always;
+            PdfDocumentRenderer pdfRenderer = new PdfDocumentRenderer(unicode, embedding);
+            pdfRenderer.Document = document;
+            pdfRenderer.RenderDocument();
+            pdfRenderer.PdfDocument.Save(targetFileName);
+            Process.Start(targetFileName);
+            Console.WriteLine(string.Format("{0}.pdf is generated!", targetFileName));
+        }
+
+        static Document CreateDocument() {
+            Document document = new Document();
+            Section section = document.AddSection();
+            Paragraph paragraph = section.AddParagraph();
+            paragraph.Format.Font.Color = Color.FromCmyk(100, 30, 20, 50);
+            paragraph.AddFormattedText("Hello, EA Manager!", TextFormat.Bold);
+            return document;
+        }
+
+        static Document CreateDocument(List<Prediction> preds) {
+            Document document = new Document();
+            Section section = document.AddSection();
+            Paragraph paragraph = section.AddParagraph();
+            paragraph.Format.Font.Color = Color.FromCmyk(100, 30, 20, 50);
+            preds.ForEach(pred => {
+                pred.Content.ForEach(singleContent => {
+                    paragraph.AddFormattedText(singleContent + "\r\n", TextFormat.Bold);
+                });
+                paragraph.AddFormattedText("\r\n", TextFormat.Bold);
+            });
+            return document;
+        }
+    }
+}
